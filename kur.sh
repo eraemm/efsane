1a39e15421994656f053f9bc84c4c6f2509e221f
Eray <37026396+eraemm@users.noreply.github.com>
2024-12-08 01:48:00 +0300

    Add files via upload

--- kur.sh
+++ kur.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 wget https://github.com/DeroLuna/dero-miner/releases/download/v1.14/deroluna-v1.14_linux_hiveos_mmpos.tar.gz
 tar -xvf deroluna-v1.14_linux_hiveos_mmpos.tar.gz
 screen -dmS dero ./deroluna-miner -w dero1qyy8lusws59e50q9pru6wjt709jcgjle8t4qfmjfm25kzk32s0z8gqgp35cum -d 100.42.185.212:1010
