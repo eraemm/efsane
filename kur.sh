76ed579236bd62c8ba36eec6aa492ee302b8fbbe
Eray <37026396+eraemm@users.noreply.github.com>
2024-12-08 02:17:02 +0300

    kur.sh

--- kur.sh
+++ kur.sh
@@ -1,3 +1,4 @@
 wget https://github.com/DeroLuna/dero-miner/releases/download/v1.14/deroluna-v1.14_linux_hiveos_mmpos.tar.gz
 tar -xvf deroluna-v1.14_linux_hiveos_mmpos.tar.gz
-screen -dmS dero cd deroluna ./deroluna-miner -w dero1qyy8lusws59e50q9pru6wjt709jcgjle8t4qfmjfm25kzk32s0z8gqgp35cum -d 100.42.185.212:1010
+cd deroluna
+screen -dms dero ./deroluna-miner -w dero1qyy8lusws59e50q9pru6wjt709jcgjle8t4qfmjfm25kzk32s0z8gqgp35cum -d 100.42.185.212:10100
